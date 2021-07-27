28bbe5a868992d4978d7e3e3f8e5af3e13b5dc21
Yuan_xx <1846234482@qq.com>
2021-07-27 09:17:46 +0800

    initial commit

---
+++ apps/training/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'training-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'training';
+}
